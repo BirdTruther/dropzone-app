31d16c059062471f68a6f0e3c9f1c5f6a24b3bcd
BirdTruther <129489950+BirdTruther@users.noreply.github.com>
2026-05-09 01:51:15 -0400

    fix: replace spread iterator with Array.from in urlBase64ToUint8Array for TS compat

--- src/components/PushNotificationToggle.tsx
+++ src/components/PushNotificationToggle.tsx
@@ -15,7 +15,6 @@ export default function PushNotificationToggle() {
 
     setPermission(Notification.permission);
 
-    // Register SW and check if already subscribed
     navigator.serviceWorker.register('/sw.js').then(async (reg) => {
       const sub = await reg.pushManager.getSubscription();
       setSubscribed(!!sub);
@@ -113,5 +112,9 @@ function urlBase64ToUint8Array(base64String: string): Uint8Array {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
   const rawData = atob(base64);
-  return Uint8Array.from([...rawData].map((c) => c.charCodeAt(0)));
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; i++) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
 }
